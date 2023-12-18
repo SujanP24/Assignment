36a41a647b9b632ba58f08450fcca8303d8ad456
SujanP24 <117675900+SujanP24@users.noreply.github.com>
2023-12-18 10:32:56 +0530

    Update SwaggerConfig.java

--- demo/src/main/java/com/assignment/demo/configuration/SwaggerConfig.java
+++ demo/src/main/java/com/assignment/demo/configuration/SwaggerConfig.java
@@ -17,16 +17,6 @@ import java.util.List;
 @Configuration
 public class SwaggerConfig {
 
-//    @Bean
-//    public OpenApiCustomiser openApiCustomiser() {
-//        return openApi -> openApi
-//                .info(new Info().title("Your API Title").version("1.0").description("Your API Description"))
-//                .externalDocs(new ExternalDocumentation().description("Your External Documentation Description").url("Your External Documentation URL"))
-//                .servers(List.of(new Server().url("http://localhost:8080").description("Your Server Description")))
-//                .components(new Components())
-//                .tags(List.of(new Tag().name("Your Tag Name").description("Your Tag Description")))
-//                .addSecurityItem(new SecurityRequirement().addList("Your Security Schema Name"));
-//    }
 
     @Bean
     public GroupedOpenApi actuatorApi() {
